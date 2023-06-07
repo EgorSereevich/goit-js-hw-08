7eeb55d7e3389e7ff0cd382778fd013863e61d45
EgorSereevich <122815068+EgorSereevich@users.noreply.github.com>
2023-06-07 10:42:16 +0000

    Deploying to gh-pages from @ EgorSereevich/goit-js-hw-08@351fa6c09ff14a15e7f17b37ae0bfecd9e41a640 🚀

---
+++ 03-feedback.47134a78.js
@@ -0,0 +1,2 @@
+console.log("zalupa"),localStorage.getItem("zalupa","polnaya"),console.log();
+//# sourceMappingURL=03-feedback.47134a78.js.map
---
+++ 03-feedback.8458b5e1.js
@@ -0,0 +1,2 @@
+console.log("zalupa"),localStorage.getItem("zalupa","polnaya"),console.log();
+//# sourceMappingURL=03-feedback.8458b5e1.js.map
