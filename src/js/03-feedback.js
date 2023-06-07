a8533a26a7f5371d577d1c216fc3b85e8819bc54
Egor Sergeevich <122815068+EgorSereevich@users.noreply.github.com>
2023-06-07 15:51:19 +0300

    result

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,43 @@
-console.log('zalupa');
-localStorage.getItem('zalupa', 'polnaya');
-console.log();
+import throttle from 'lodash.throttle';
+const formEl = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('.feedback-form input');
+const textEl = document.querySelector('.feedback-form textarea');
+const buttonEl = document.querySelector('.feedback-form button ');
+const formData = {};
+const zeroForm = '';
+const STR_KEY = 'feedback-form-state';
+onReturnForm();
+function onReturnForm() {
+  const form = JSON.parse(localStorage.getItem(STR_KEY));
+  if (form) {
+    inputEl.value = form.email;
+    textEl.value = form.message;
+  }
+}
+formEl.addEventListener('input', throttle(onFormInput, 500));
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STR_KEY, JSON.stringify(formData));
+}
+
+formEl.addEventListener('submit', onRemoveForm);
+function onRemoveForm(evt) {
+  evt.preventDefault();
+  if (
+    evt.target.elements.email.value === '' ||
+    evt.target.elements.message.value === ''
+  ) {
+    return alert('Заповніть усі поля');
+  }
+  console.log(JSON.parse(localStorage.getItem(STR_KEY)));
+  localStorage.removeItem(STR_KEY);
+  evt.currentTarget.reset();
+}
+function returnFeedback() {
+  const preMassage = JSON.parse(localStorage.getItem(STR_KEY));
+
+  if (preMassage) {
+    inputEl.value = preMassage.email;
+    textEl.value = preMassage.message;
+  }
+}
